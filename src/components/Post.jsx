ee4b3c6c314240b9d815efe06d34d24cbcc4bd2d
Anton Öhman <anton.ohman@hotmail.com>
2020-05-11 15:10:47 +0200

    Styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "../css/post.css";
 
+let date = new Date()
+
 function Post(props) {
   return (
     <div className="post">
       <div>
           <img className="postImg" src={props.imageSource} />
       </div>
-      <div>
+      <div className="postInfo">
         <h3>{props.title}</h3>
-        <h6>{props.date}</h6>
-        <h4>{props.description} </h4>
+        <h6>Datum: {date.getMonth() + 1}/{date.getDate()} - {date.getFullYear()}</h6>
+        <p>{props.description} </p>
       </div>
     </div>
   );
--- src/components/Startpage.jsx
+++ src/components/Startpage.jsx
@@ -8,13 +8,15 @@ import ProfileSidebar from './ProfileSidebar'
 function Startpage() {
   return (
         <div className="startpageContainer">
-            <h1 className="startpageTitle">Coop Forum!</h1>
+            <div className="startpageHeader">
+                <h1 className="startpageTitle">Coop Forum!</h1>
+                <ProfileSidebar />
+            </div>
             <Post
             imageSource="https://images.unsplash.com/photo-1515542743364-f002238fe5f8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-            title={'Mitt nya husdjur'}
-            // date={Date.toDateString()} 
-            description={'Hej alla glada! Här är en bild på mitt nya husdjur. Jag tycker han är fin, alltså inte ful.'} />
-           <ProfileSidebar />
+            title={'Mitt nya husdjur'} 
+            description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis, turpis nec imperdiet consequat, velit eros fermentum nulla, at tempor metus erat finibus nulla. Pellentesque blandit augue et lectus tincidunt aliquet. Praesent iaculis sollicitudin nibh. Mauris bibendum tempus nibh, non convallis sapien varius sit amet. Proin pellentesque enim at rhoncus consequat. Donec tempus mattis volutpat. Vestibulum eget accumsan nunc, a tempor diam. Proin eu gravida enim. Fusce hendrerit, mi a finibus rutrum, elit leo tristique elit, id commodo massa velit ut odio. Suspendisse cursus nisl in cursus feugiat. Curabitur auctor fringilla ipsum, eu venenatis diam tincidunt at. Morbi quis erat justo.'} />
+           
         </div>
     )
 }
