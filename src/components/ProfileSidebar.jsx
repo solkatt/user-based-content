240285bc86ccf1a995610947208733bc11f64f43
Viktor Ödman <viktoralexanderodman@gmail.com>
2020-05-12 11:55:13 +0200

    login-system

--- src/components/ProfileSidebar.jsx
+++ src/components/ProfileSidebar.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import "../css/profileSidebar.css";
-
+import '../css/profileSidebar.css'
 
 function ProfileSidebar(props) {
-    return(
-        <div className="profileSidebar">
-            <h3>Hej Kalle! </h3>
-            <button type="button">Your Posts</button>
-            <button type="button">Create New Post</button>
-
-            </div>
-    )
+	return (
+		<div className='profileSidebar'>
+			<h3>Hej Kalle! </h3>
+			<button type='button'>Your Posts</button>
+			<button type='button'>Create New Post</button>
+		</div>
+	)
 }
 
 export default ProfileSidebar
