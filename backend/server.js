f6addd216eed0ee9b18ae22a4a88705b79d67b1c
Jonathan G. Bogg <jonathan.bogg@gmail.com>
2020-05-13 14:39:12 +0200

    organize code, add image file storage, require post routes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const GridFsStorage = require("multer-gridfs-storage")
 const cors = require("cors")
 const crypto = require("crypto")
 
-
 const port = process.env.PORT || 3001
 const host = process.env.HOST || 'localhost'
 
@@ -16,10 +15,12 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors({ origin: "http://localhost:3000" }))
+app.use(cors())
+app.use(express.json())
+// On production build
+app.use(express.static('build'))
 
-const signin = require('./routes/signin')
-const currentUser = require('./routes/getCurrentUser')
+
 
 mongoose.connect(
     'mongodb://localhost:27017/co-op-forum',
@@ -37,41 +38,19 @@ mongoose.set('useFindAndModify', false)
 
 const db = mongoose.connection
 
-// Test User
-/*
-const newUser = new User({
-    username: 'CooltNamn',
-    password: 'cooltlösenord'
-})
-
-// Test save User to database
-newUser.save(function (error, newUser) {
-    if (error) {
-        return console.log(`
-        Error name: ${error.name}
-        Error message: ${error._message}`)
-    }
-    console.log('Successfully saved new user!')
-});
-*/
+// For initializing gridFsStorage (image storage)
 let gfs
-
-app.use(express.json())
 
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
+    // Create storage collection
     gfs = Grid(db.db, mongoose.mongo)
     gfs.collection('images')
     require('./routes/postRoutes')(app, gfs, upload)
     console.log('Connected to db with Mongoose!')
 })
 
-app.use(express.static('build'))
-
-app.use('/api/account/', signin)
-app.use('/api/account/user', currentUser)
-
-
+// Configuration for image file storage with multer
 const storage = new GridFsStorage({
     url: "mongodb://localhost:27017/co-op-forum",
     file: (req, file) => {
@@ -81,6 +60,7 @@ const storage = new GridFsStorage({
                     return reject(err)
                 }
                 const filename = file.originalname
+                // Saved with original filename in (bucketname).chunks and (bucketname).files
                 const fileInfo = {
                     filename: filename,
                     bucketName: 'images',
@@ -92,6 +72,13 @@ const storage = new GridFsStorage({
 })
 
 const upload = multer({ storage })
+
+// Routes
+const signin = require('./routes/signin')
+const currentUser = require('./routes/getCurrentUser')
+
+app.use('/api/account/', signin)
+app.use('/api/account/user', currentUser)
 
 app.get('/test', (req, res) => {
     res.send('TJEENA')
