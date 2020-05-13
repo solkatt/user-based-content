826331f836ce56e9b4f421403e460fb2ff857488
Jonathan G. Bogg <jonathan.bogg@gmail.com>
2020-05-13 19:34:09 +0200

    add page for all user posts

---
+++ src/components/UserPostspage/UserPostspage.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import './userpostspage.css'
+import Post from '../Post/Post'
+
+class UserPostspage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true,
+            postsJSON: [],
+            postsElements: [],
+            userId: this.props.user.userId
+        }
+    }
+
+    async fetchPosts() {
+        await fetch(`http://localhost:3001/api/post/${this.props.user.userId}`, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then((json) => {
+                this.setState({
+                    isLoading: false,
+                    postsJSON: json.allUserPosts,
+                    postsElements: this.renderPosts(json.allUserPosts)
+                })
+            }).catch(error => console.log(error))
+
+    }
+
+    /**
+     * Create post elements from fetched data
+     */
+    renderPosts = (posts) => {
+        const allPosts = posts.map(post => { return <Post key={post._id} data={post} /> })
+        return allPosts
+    }
+
+    /**
+     * Fetch posts when component mounts, will trigger rerender
+     */
+    componentDidMount() {
+        this.fetchPosts()
+    }
+
+    render() {
+        const { isLoading, postsElements: posts, userId } = this.state
+
+        return (
+            isLoading ? <p>Loading...</p>
+                : <div className="userPostsContainer">
+                    <p>Hello {userId}</p>
+                    {posts}
+                </div>
+
+        )
+    }
+}
+
+export default UserPostspage
